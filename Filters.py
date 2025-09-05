3fb69adaac1e5815a3a42439a83cec2d6662cf1b
Artur Davletshin <davletshin1984@mail.ru>
2025-09-05 11:06:04 +0500

    Добавлена функция my_start_filter, которая фильтрует только команду "/start".

--- Filters.py
+++ Filters.py
@@ -4,16 +4,19 @@ from aiogram.filters import Command
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
-BOT_TOKEN = ''
+BOT_TOKEN = '7393565379:AAF0eftofZCKOuUHWCcmO2gPeSkbhYyw6xo'
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+def my_start_filter(message: Message):
+    return message.text and message.text == '/start'
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands='start'))
+@dp.message(my_start_filter)
 async def process_start_command(message: Message):
+    print(message.model_dump_json(indent=4, exclude_none=True))
     await message.answer(text='Это команда /start')
 
 
